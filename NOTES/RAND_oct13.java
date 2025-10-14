dbe63a383254281cf7fecc01e3c734f956f506a0
speedskater1610 <speedskater1610@gmail.com>
2025-10-14 07:40:31 -0400

    Update RAND_oct13.java

--- NOTES/RAND_oct13.java
+++ NOTES/RAND_oct13.java
@@ -34,9 +34,7 @@ public class RAND_oct13 {
         //  {                 }      (0,6]
     //      {                   }    (1,7]
     //  {                       }    (1,6]
-
-
-      // but [0,1) in the real world is (0,0)
+    
     
     // we want [-3,3]
     int num2 =
@@ -55,24 +53,31 @@ public class RAND_oct13 {
 //       {                  }   (-3,4)
 //  {                       }   (-2,3)
 
+
+        // That is the way Mr.I teaches them
+        // this is the way that I prefer and I think this is a lot easier to memorize this formula
+        int max = 6;
+        int min = 13;
+        int randomNumber = (int) (Math.random() * (max - min + 1)) + min;
+        
         // examples 
         
         // [1,8]
-        int num4 = (int)(Math.random()*7) + 1;
+        int num4 = (int) (Math.random() * 8)  + 1;
         
         // [2,11]
-        int num5 = (int)(Math.random()*9) + 2;
+        int num5 = (int) (Math.random() * 10) + 2;
          
         // [0,5]
-        int num6 = (int)(Math.random()*5) + 0;
+        int num6 = (int) (Math.random() * 6) + 0;
         
         // [-2, 2]
-        int num7 = (int)(Math.random()*5) - 2;
+        int num7 = (int) (Math.random() * 5) - 2;
         
         // [-7, -1]
-        int num8 = (int)(Math.random()*7) - 7;
+        int num8 = (int) (Math.random() * 7) - 7;
         
         // [67]
-        int num9 = (int)Math.random() + 68;
+        int num9 = (int) (Math.random() * 1) + 67;
     }
 }
