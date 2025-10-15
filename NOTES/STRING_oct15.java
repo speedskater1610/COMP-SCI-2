945dbdb3a17fa7af7ffeac1a8e039081799ded0a
speedskater1610 <speedskater1610@gmail.com>
2025-10-15 08:26:51 -0400

    Update STRING_oct15.java

--- NOTES/STRING_oct15.java
+++ NOTES/STRING_oct15.java
@@ -1,5 +1,39 @@
-public class STING_oct15 {
+public class STRING_oct15 {
     public static void main (String [] args) {
+        /*Strings are objects*/
+
+        /* 
+        objects are a collection of methods and properties. 
+        a string is a sequence of characters
+        from the ascii table *UNICODE MR.I!!*
+        the syntax used for string values is "" (double quotes)
+
+        There are a varity of String methods...
+
+        = = = = = Indexing & Len = = = = =
+        every char of a string has an index value
+        The index is the location of the character
+        in the String.
+        Indexes start at index 0
+
+        Ex.    String name = "Thomas";
+        index:                012345
+
+
+        the length of a string is the number of characters it contains
+        Ex.   String name = "Thomas";
+        name.length();      =>      6
         
+        = = = = = String methods = = = = =
+        Common String methods
+            String thing = "Eli";
+            thing.toUpperCase();        returns => "ELI"
+            thing.toLowerCase();        returns => "eli"
+            thing.charAt(a);            returns => the char at index a (int)
+                thing.charAt(1);            => 'l'
+            thing.indexOf(str);         returns => the index of the first occourence str
+                                                returns => -1 if str isnt found;
+            thing.substring();
+        */
     }
 }
